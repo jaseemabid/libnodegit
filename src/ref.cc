d83ece1fcfac32925f9a9472702a02c7c9f80291
Jaseem Abid <jaseemabid@gmail.com>
2012-10-07 19:46:24 +0530

    Implementing repo.head():String

--- src/ref.cc
+++ src/ref.cc
@@ -14,6 +14,24 @@ Handle<Value> Repository::head(const Arguments& args) {
 	HandleScope scope;
 
 	Repository* obj = ObjectWrap::Unwrap<Repository>(args.This());
+	git_reference * head_out;
 
-	return scope.Close(String::New("head"));
+	int status;
+
+	status = git_repository_head(&head_out, obj->repo_);
+
+	if (status == 0) {
+
+		const git_oid * head_oid = git_reference_oid(head_out);
+
+		char out[41];
+		out[40] = '\0';
+		git_oid_fmt(out, head_oid);
+
+		return scope.Close(String::New(out));
+
+	} else {
+
+		return ThrowException(Exception::Error(String::New("Unable to open the git repo")));
+	}
 }
