1de0f007cbfb661f4075601b3139a18c72a0b0e4
Jaseem Abid <jaseemabid@gmail.com>
2013-03-23 02:42:39 +0530

    Make util function definitions inline
    Ugly redefinition errors which I don't really understand. This works for
    now, so ship and iterate mate!

--- src/util.h
+++ src/util.h
@@ -5,14 +5,14 @@
 
 using namespace v8;
 
-char* V8StringToChar(Handle<String> str) {
+inline char* V8StringToChar(Handle<String> str) {
   int len = str->Utf8Length();
   char* buf = new char[len + 1];
   str->WriteUtf8(buf, len + 1);
   return buf;
 }
 
-char* V8StringToChar(Local<Value> val) {
+inline char* V8StringToChar(Local<Value> val) {
   return V8StringToChar(val->ToString());
 }
 
